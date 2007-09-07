659b83aae3c0f4dad6a301273aeb6cefa43bbfa8
Christopher Wellons <mosquitopsu@gmail.com>
2007-09-07 04:45:34 +0000

    Added copyright headers to all files.

--- mandel.c
+++ mandel.c
@@ -1,9 +1,10 @@
 /* mandelbrot parallel fractal generator
- * Copyright (C) 2007 Christopher Wellons (mosquitopsu@gmail.com)
- * 
+ * Copyright (C) 2007 Christopher Wellons <ccw129@psu.edu>
+ *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License version
- * 3 as published by the Free Software Foundation.
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,7 +12,9 @@
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  */
 
 #include <stdio.h>
